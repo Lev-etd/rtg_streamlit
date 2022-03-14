124da6e0f8dfbf899c8fc3363c7f462e4fe3d694
Lev-etd <levevt2012@gmail.com>
2022-03-14 04:16:02 +0300

    minor changes

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 git clone https://github.com/Lev-etd/rtg_streamlit.git
+cd rtg_streamlit/
 apt install wget
 pip install flask
 pip install rtg==0.6.1
 pip install streamlit
-cd rtg_streamlit/
 wget http://rtg.isi.edu/many-eng/models/rtg500eng-tfm9L6L768d-bsz720k-stp200k-ens05.tgz
 tar xvf rtg500eng-tfm9L6L768d-bsz720k-stp200k-ens05.tgz
 cp conf.yml -fr rtg500eng-tfm9L6L768d-bsz720k-stp200k-ens05/conf.yml
